148f80793734f0b8f378c26dc16490941ca95a4e
soulnaya <sugdiyona711@gmail.com>
2023-02-06 03:54:45 -0800

    practice2

--- practice2/script.js
+++ practice2/script.js
@@ -2,6 +2,22 @@
 alert("hello");
 //practice
 
-let ogirlik= Number(prompt(1));
-let boy=Number(prompt(2));
-console.log(ogirlik / boy**2);
+let ogirlik= Number(prompt("kg"));
+let boy=Number(prompt("m"));
+let c =ogirlik / boy**2;
+
+if(c<18.5){
+    alert("underwegiht")
+}
+else if (18.5<=c && c<=24.9){
+alert("normal")
+}
+else if(24.9<=c && c<=25){
+    alert("overwegiht")
+}
+else if(25<=c && c<=30){
+    alert("obese")
+}
+else if(30<=c<=35){
+    alert("extremely obese")
+}
